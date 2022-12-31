9e9f7ded5bfb84034b6a863482c921a6a5cc7fe2
Sigma <sigmacoolgamer@gmail.com>
2022-12-31 04:00:31 -0500

    feat: class based account handling

---
+++ account.js
@@ -0,0 +1,171 @@
+const fs = require('fs')
+const puppeteer = require('puppeteer-extra')
+const output = require('./output')
+const { executablePath } = require('puppeteer')
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const CapSolver = require('capsolver-npm')
+const handler = new CapSolver(process.env.API_KEY, 0)
+
+class account {
+  constructor(email, password, birthday, proxy, name = { first, last }) {
+    this.proxy = proxy
+    this.name = name
+
+    this.birthday = {
+      formatted: `${(birthday.getMonth() + 1 ).toString()}/${birthday.getDay().toString()}/${birthday.getFullYear().toString()}`,
+      raw: birthday
+    }
+
+    this.email = {
+      formatted: `${email}@outlook.com`,
+      raw: email
+    }
+
+    this.password = {
+      censored: password.replace(/./g, '*'),
+      raw: password
+    }
+  }
+
+  async getBalance() {
+    const balance = await handler.balance()
+    this.balance = balance
+
+    return balance
+  }
+
+  async init(options = { headless, slowMo }) {
+    output.info('Initializing new account...')
+
+    const browser = await puppeteer.launch({ 
+      args: [
+        '--no-sandbox',
+        '--disable-web-security',
+        '--disable-site-isolation-trials',
+        '--disable-application-cache',
+        `--proxy-server=${this.proxy[0]}:${this.proxy[1]}`
+      ],
+      headless: options.headless,
+      executablePath: executablePath(),
+      slowMo: options.slowMo
+    })
+
+    this.browser = browser
+
+    const page = await browser.newPage()
+
+    await page.authenticate({
+      username: this.proxy[2],
+      password: this.proxy[3]
+    })
+
+    output.info('Navigating to signup page...')
+    await page.goto('https://signup.live.com/signup', {
+      waitUntil: 'domcontentloaded'
+    })
+
+    output.info('Entering email...', `${this.email.formatted}`)
+
+    await page.waitForSelector('#liveSwitch');
+    await page.click('#liveSwitch')
+
+    await page.type('#MemberName', this.email.raw);
+    await page.click('#iSignupAction');
+
+    output.info('Entering password...', this.password.censored)
+    await page.waitForSelector('#PasswordInput');
+    await page.type('#PasswordInput', this.password.raw);
+    await page.click('#iSignupAction');
+
+    output.info('Entering name...', `${this.name.first} ${this.name.last}`)
+
+    await page.waitForSelector('#FirstName');
+    await page.type('#FirstName', this.name.first)
+    await page.type('#LastName', this.name.last);
+    await page.click('#iSignupAction');
+    
+    output.info('Entering birthday...', this.birthday.formatted)
+
+    await page.waitForSelector('#BirthMonth');
+    await page.select('select#BirthMonth', (this.birthday.raw.getMonth() + 1).toString())
+    await page.waitForSelector('#BirthDay');
+    await page.select('select#BirthDay', this.birthday.raw.getDate().toString())
+    await page.click('#BirthYear')
+    await page.keyboard.type(this.birthday.raw.getFullYear().toString());
+    await page.click('#iSignupAction')
+
+    var solvedFc;
+
+    const phoneLocked = await page.waitForSelector('#HipPaneForm > div.text-title.forSmsHip', {
+      timeout: 3 * 1000
+    }).catch(e => {})
+
+    if (phoneLocked) {
+      output.error('Blocked by Microsoft!', 'Error: Phone verification required')
+      output.warn('Try switching your proxies!')
+
+      return browser.close()
+    }
+
+    try {
+      await page.waitForSelector('#enforcementFrame');
+      output.info('Requesting FunCaptcha solution from server...')
+      
+      solvedFc = await handler.funcaptcha(
+        'https://signup.live.com/signup', 
+        'B7D8911C-5CC8-A9A3-35B0-554ACEE604DA',
+        {
+          proxyType: 'http',
+          proxyAddress: this.proxy[0],
+          proxyPort: Number(this.proxy[1]),
+          proxyLogin: this.proxy[2],
+          proxyPassword: this.proxy[3]
+        },
+        'client-api.arkoselabs.com', 
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
+      )
+  
+      if (solvedFc.error == -1) throw new Error(solvedFc.apiResponse.errorDescription)
+    } catch (e) {
+      output.error('Captcha failed!', e)
+      return browser.close()
+    }
+
+    output.success('Received solution!')
+
+    output.info('Injecting FunCaptcha solution to page source...')
+    await page.evaluate(token => {
+      var enc = document.getElementById('enforcementFrame');
+      var encWin = enc.contentWindow || enc;
+      var encDoc = enc.contentDocument || encWin.document;
+      let script = encDoc.createElement('SCRIPT');
+      script.append('function captchaSubmit(token) { parent.postMessage(JSON.stringify({ eventId: "challenge-complete", payload: { sessionToken: token } }), "*") }');
+      encDoc.documentElement.appendChild(script);
+      encWin.captchaSubmit(token);
+    }, solvedFc.solution.token)
+
+    try {
+      await page.waitForSelector('#idSIButton9', {
+        timeout: 10 * 1000
+      })
+    } catch (e) {
+      output.error('Captcha injection failed!', e)
+      output.warn('Your proxy may have been blocked!', 'Must be HQ')
+  
+      return browser.close()
+    }
+    
+    await page.click('#idSIButton9');
+    output.success('Successfully registered!', `${this.name.first} ${this.name.last}`, this.email.formatted, this.password.censored, this.birthday.formatted)
+    
+    fs.appendFileSync('accounts.txt', `\n${email}@outlook.com:${password}`);
+
+    const accounts = fs.readFileSync('accounts.txt', 'utf8').split('\n').length
+    output.info('The account has been saved!', `You now have: ${accounts} accounts`)
+  }
+}
+
+module.exports = account
--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-// TODO:
-//  - Fix birthdate month/day selection glitch
-//  - Auto-redeem gamepass codes
-
 require('dotenv').config()
 
-const puppeteer = require('puppeteer-extra')
+const fs = require('fs')
 const Chance = require('chance')
-const chance = new Chance()
-const fs = require('fs');
+const Account = require('./account')
 const output = require('./output')
-
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
-
-const CapsolverPlugin = require('puppeteer-extra-plugin-capsolver')();
-CapsolverPlugin.setHandler(process.env.API_KEY, 0)
-puppeteer.use(CapsolverPlugin);
-
-const {executablePath} = require('puppeteer')
+const chance = new Chance();
 
 function getRandomProxy() {
   const fileContents = fs.readFileSync('proxies.txt', 'utf-8');
@@ -28,137 +15,30 @@ function getRandomProxy() {
   return proxies[randomIndex].split(':');
 }
 
-const proxy = getRandomProxy()
+(async () => {
+  const proxy = getRandomProxy()
+  const email = chance.letter() + chance.hash({ length: 14, casing: 'lower' })
+  const password = 'PLACE' + chance.hash({ length: 15, casing: 'upper' })
+  const birthday = chance.birthday()
+  const firstName = chance.first()
+  const lastName = chance.last()
 
-output.info('Proxy loaded!', `${proxy[0]}:${proxy[1]}`)
+  const account = new Account(
+    email,
+    password,
+    birthday,
+    proxy,
+    {
+      first: firstName,
+      last: lastName
+    }
+  )
 
-puppeteer.launch({ 
-  args: [
-    '--no-sandbox',
-    '--disable-web-security',
-    '--disable-site-isolation-trials',
-    '--disable-application-cache',
-    `--proxy-server=${proxy[0]}:${proxy[1]}`
-  ],
-  //headless: false,
-  executablePath: executablePath(),
-  slowMo: 0
-}).then(async browser => {
-  const balance = await CapsolverPlugin.handler().balance()
+  const balance = await account.getBalance()
 
-  output.success('Successfully fetched account balance!', `$${balance}`)
-  output.success('Starting...')
+  output.success('Successfully accessed CapSolver account!', `$${balance}`)
 
-  const page = await browser.newPage()
-
-  await page.authenticate({
-    username: proxy[2],
-    password: proxy[3]
+  await account.init({
+    headless: false
   })
-
-  output.info('Navigating to signup page...')
-  await page.goto('https://signup.live.com/signup', {
-    waitUntil: 'domcontentloaded'
-  })
-
-  var email = chance.letter() + chance.hash({ length: 14, casing: 'lower' })
-  var password = 'PLACE' + chance.hash({ length: 15, casing: 'upper' })
-
-  output.info('Entering email...', `${email}@outlook.com`)
-
-  await page.waitForSelector('#liveSwitch');
-  await page.click('#liveSwitch');
-  await page.keyboard.type(email);
-  await page.click('#iSignupAction');
-
-  output.info('Entering password...', password.replace(/./g, '*'))
-  await page.waitForSelector('#PasswordInput');
-  await page.type('#PasswordInput', password);
-  await page.click('#iSignupAction');
-
-  var firstName = chance.first()
-  var lastName = chance.last()
-
-  output.info('Entering name...', `${firstName} ${lastName}`)
-
-  await page.waitForSelector('#FirstName');
-  await page.type('#FirstName', firstName)
-  await page.type('#LastName', lastName);
-  await page.click('#iSignupAction');
-
-  var birthday = chance.birthday()
-
-  output.info('Entering birthday...', `${birthday.getMonth().toString()}/${birthday.getDay().toString()}/${birthday.getFullYear().toString()}`)
-
-  async function setBirthday(page, birthday) {
-    await page.waitForSelector('#BirthMonth');
-    await page.select('select#BirthMonth', birthday.getMonth().toString())
-    await page.waitForSelector('#BirthDay');
-    await page.select('select#BirthDay', birthday.getDay().toString())
-    await page.click('#BirthYear')
-    await page.keyboard.type(birthday.getFullYear().toString());
-    await page.click('#iSignupAction')
-
-    const failedInput = await page.$('#BirthDateError > div');
-    if (failedInput) await setBirthday(page, birthday)
-  }
-
-  await setBirthday(page, birthday)
-
-  const failedInput = await page.$('#BirthDateError > div');
-  if (failedInput) await setBirthday(page, birthday)
-
-  var solvedFc;
-
-  try {
-    output.info('Requesting FunCaptcha solution from server...')
-    
-    solvedFc = await CapsolverPlugin.handler().funcaptcha(
-      'https://signup.live.com/signup', 
-      'B7D8911C-5CC8-A9A3-35B0-554ACEE604DA',
-      {
-        proxyType: 'http',
-        proxyAddress: proxy[0],
-        proxyPort: Number(proxy[1]),
-        proxyLogin: proxy[2],
-        proxyPassword: proxy[3]
-      },
-      'client-api.arkoselabs.com', 
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
-    )
-
-    if (solvedFc.error == -1) throw new Error(solvedFc.apiResponse.errorDescription)
-  } catch (e) {
-    output.error('Captcha failed!', e)
-    return browser.close()
-  }
-
-  output.success('Received solution!')
-
-  await page.waitForSelector('#enforcementFrame');
-  output.info('Injecting FunCaptcha solution to page source...')
-  await page.evaluate(token => {
-    var enc = document.getElementById('enforcementFrame');
-    var encWin = enc.contentWindow || enc;
-    var encDoc = enc.contentDocument || encWin.document;
-    let script = encDoc.createElement('SCRIPT');
-    script.append('function captchaSubmit(token) { parent.postMessage(JSON.stringify({ eventId: "challenge-complete", payload: { sessionToken: token } }), "*") }');
-    encDoc.documentElement.appendChild(script);
-    encWin.captchaSubmit(token);
-  }, solvedFc.solution.token)
-
-  try {
-    await page.waitForSelector('#idSIButton9', {
-      timeout: 15 * 1000
-    })
-  } catch (e) {
-    output.error('Captcha injection failed!', 'Error: Solution invalidated', 'Proxy most likely blocked by FunCaptcha')
-
-    return browser.close()
-  }
-
-  await page.click('#idSIButton9');
-  fs.appendFileSync('accounts.txt', `\n${email}@outlook.com:${password}`);
-  output.success('Successfully registered!', `${firstName} ${lastName}`, `${email}@outlook.com`, password.replace(/./g, '*'), `${birthday.getMonth().toString()}/${birthday.getDay().toString()}/${birthday.getFullYear().toString()}`, )
-  browser.close()
-})
+})();
