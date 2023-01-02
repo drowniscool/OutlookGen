fbca03f273c487c4ebc8e697a1b0ab6e515fcb71
Sigma <sigmacoolgamer@gmail.com>
2023-01-01 20:08:47 -0500

    fix: seperate solving proxy from signup proxies

--- account.js
+++ account.js
@@ -10,8 +10,9 @@ const CapSolver = require('capsolver-npm')
 const handler = new CapSolver(process.env.API_KEY, 0)
 
 class account {
-  constructor(email, password, birthday, proxy, name = { first, last }) {
+  constructor(email, password, birthday, proxy, captchaProxy, name = { first, last }) {
     this.proxy = proxy
+    this.captchaProxy = captchaProxy
     this.name = name
 
     this.birthday = {
@@ -38,7 +39,7 @@ class account {
   }
 
   async init(options = { headless, slowMo }) {
-    output.info('Initializing new account...')
+    output.info(this.name.first, 'Initializing...')
 
     const browser = await puppeteer.launch({ 
       args: [
@@ -62,12 +63,12 @@ class account {
       password: this.proxy[3]
     })
 
-    output.info('Navigating to signup page...')
+    output.info(this.name.first, 'Navigating to signup page...')
     await page.goto('https://signup.live.com/signup', {
       waitUntil: 'domcontentloaded'
     })
 
-    output.info('Entering email...', `${this.email.formatted}`)
+    output.info(this.name.first, 'Entering email...', `${this.email.formatted}`)
 
     await page.waitForSelector('#liveSwitch');
     await page.click('#liveSwitch')
@@ -75,19 +76,19 @@ class account {
     await page.type('#MemberName', this.email.raw);
     await page.click('#iSignupAction');
 
-    output.info('Entering password...', this.password.censored)
+    output.info(this.name.first, 'Entering password...', this.password.censored)
     await page.waitForSelector('#PasswordInput');
     await page.type('#PasswordInput', this.password.raw);
     await page.click('#iSignupAction');
 
-    output.info('Entering name...', `${this.name.first} ${this.name.last}`)
+    output.info(this.name.first, 'Entering name...', `${this.name.first} ${this.name.last}`)
 
     await page.waitForSelector('#FirstName');
     await page.type('#FirstName', this.name.first)
     await page.type('#LastName', this.name.last);
     await page.click('#iSignupAction');
     
-    output.info('Entering birthday...', this.birthday.formatted)
+    output.info(this.name.first, 'Entering birthday...', this.birthday.formatted)
 
     await page.waitForSelector('#BirthMonth');
     await page.select('select#BirthMonth', (this.birthday.raw.getMonth() + 1).toString())
@@ -104,25 +105,27 @@ class account {
     }).catch(e => {})
 
     if (phoneLocked) {
-      output.error('Blocked by Microsoft!', 'Error: Phone verification required')
-      output.warn('Try switching your proxies!')
+      output.error(this.name.first, 'Blocked by Microsoft!', 'Error: Phone verification required')
+      output.warn(this.name.first, 'Try switching your proxies!')
 
-      return browser.close()
+      browser.close()
+
+      return false
     }
 
     try {
       await page.waitForSelector('#enforcementFrame');
-      output.info('Requesting FunCaptcha solution from server...')
+      output.info(this.name.first, 'Requesting FunCaptcha solution from server...')
       
       solvedFc = await handler.funcaptcha(
         'https://signup.live.com/signup', 
         'B7D8911C-5CC8-A9A3-35B0-554ACEE604DA',
         {
           proxyType: 'http',
-          proxyAddress: this.proxy[0],
-          proxyPort: Number(this.proxy[1]),
-          proxyLogin: this.proxy[2],
-          proxyPassword: this.proxy[3]
+          proxyAddress: this.captchaProxy[2],
+          proxyPort: Number(this.captchaProxy[3]),
+          proxyLogin: this.captchaProxy[0],
+          proxyPassword: this.captchaProxy[1]
         },
         'client-api.arkoselabs.com', 
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
@@ -130,13 +133,18 @@ class account {
   
       if (solvedFc.error == -1) throw new Error(solvedFc.apiResponse.errorDescription)
     } catch (e) {
-      output.error('Captcha failed!', e)
-      return browser.close()
+      output.error(this.name.first, 'Captcha failed!', e)
+      
+      browser.close()
+      
+      return false 
     }
 
-    output.success('Received solution!')
+    const balance = await this.getBalance()
 
-    output.info('Injecting FunCaptcha solution to page source...')
+    output.success(this.name.first, 'Received solution!', `$${balance}`)
+
+    output.info(this.name.first, 'Injecting FunCaptcha solution to page source...')
     await page.evaluate(token => {
       var enc = document.getElementById('enforcementFrame');
       var encWin = enc.contentWindow || enc;
@@ -152,19 +160,23 @@ class account {
         timeout: 10 * 1000
       })
     } catch (e) {
-      output.error('Captcha injection failed!', e)
-      output.warn('Your proxy may have been blocked!', 'Must be HQ')
-  
-      return browser.close()
+      output.error(this.name.first, 'Captcha injection failed!', e)
+      output.warn(this.name.first, 'Your proxy may have been blocked!', 'Must be HQ')
+      
+      browser.close()
+      
+      return false
     }
     
     await page.click('#idSIButton9');
-    output.success('Successfully registered!', `${this.name.first} ${this.name.last}`, this.email.formatted, this.password.censored, this.birthday.formatted)
+    output.success(this.name.first, 'Successfully registered!', `${this.name.first} ${this.name.last}`, this.email.formatted, this.password.censored, this.birthday.formatted)
     
-    fs.appendFileSync('accounts.txt', `\n${email}@outlook.com:${password}`);
+    fs.appendFileSync('accounts.txt', `\n${this.email.formatted}:${this.password.raw}`);
 
     const accounts = fs.readFileSync('accounts.txt', 'utf8').split('\n').length
-    output.info('The account has been saved!', `You now have: ${accounts} accounts`)
+    output.info(this.name.first, 'The account has been saved!', `${accounts} accounts`)
+
+    return true
   }
 }
 
--- app.js
+++ app.js
@@ -6,39 +6,72 @@ const Account = require('./account')
 const output = require('./output')
 const chance = new Chance();
 
-function getRandomProxy() {
+const CapSolver = require('capsolver-npm')
+const handler = new CapSolver(process.env.API_KEY, 0)
+
+let usedProxies = [];
+
+function getUnusedProxy() {
   const fileContents = fs.readFileSync('proxies.txt', 'utf-8');
   const strippedFileContents = fileContents.replace(/\r/g, '');
   const proxies = strippedFileContents.split('\n');
-  const randomIndex = Math.floor(Math.random() * proxies.length);
 
-  return proxies[randomIndex].split(':');
+  let proxy;
+  do {
+    const randomIndex = Math.floor(Math.random() * proxies.length);
+    proxy = proxies[randomIndex].split(':');
+  } while (usedProxies.includes(proxy));
+
+  usedProxies.push(proxy);
+  return proxy;
 }
 
+
 (async () => {
-  const proxy = getRandomProxy()
-  const email = chance.letter() + chance.hash({ length: 14, casing: 'lower' })
-  const password = 'PLACE' + chance.hash({ length: 15, casing: 'upper' })
-  const birthday = chance.birthday()
-  const firstName = chance.first()
-  const lastName = chance.last()
+  const balance = await handler.balance()
 
-  const account = new Account(
-    email,
-    password,
-    birthday,
-    proxy,
-    {
-      first: firstName,
-      last: lastName
+  output.success('SYSTEM', 'Successfully accessed CapSolver account!', `$${balance}`)
+
+  while (true) {
+    const proxy = getUnusedProxy()
+    const censoredProxy = proxy[0].split('.').map((section, i) => (i > 1 ? '*'.repeat(section.length) : section)).join('.');
+    
+    output.success('SYSTEM', 'Fetched unused proxy!', `${censoredProxy}:${proxy[1]}`)
+
+    const promises = [];
+    for (let i = 0; i < 5; i++) {
+      output.info('SYSTEM', 'Generating random user data...')
+
+      const email = chance.letter() + chance.hash({ length: 14, casing: 'lower' })
+      const password = 'PLACE' + chance.hash({ length: 15, casing: 'upper' })
+      const birthday = chance.birthday()
+      const firstName = chance.first()
+      const lastName = chance.last()
+      const captchaProxy = process.env.CAPTCHA_PROXY.split(':')
+    
+      const account = new Account(
+        email,
+        password,
+        birthday,
+        proxy,
+        captchaProxy,
+        {
+          first: firstName,
+          last: lastName
+        }
+      )
+    
+      promises.push(account.init({
+        headless: true
+      }));
+      await new Promise(resolve => setTimeout(resolve, 5000));
     }
-  )
+    
+    const results = await Promise.all(promises);
+    const successCount = results.filter(result => result).length;
+    output.success('SYSTEM', `Successfully created ${successCount} accounts!`);
 
-  const balance = await account.getBalance()
-
-  output.success('Successfully accessed CapSolver account!', `$${balance}`)
-
-  await account.init({
-    headless: true
-  })
+    const index = usedProxies.indexOf(proxy)
+    usedProxies.splice(index, 1)
+  }
 })();
