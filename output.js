0a3c79de5505590cb4d250f448d11841a899dfb8
Sigma <sigmacoolgamer@gmail.com>
2023-01-01 20:07:24 -0500

    feat(output): display current account username

--- output.js
+++ output.js
@@ -1,28 +1,28 @@
 const chalk = require('chalk')
 
 class output {
-  static info(msg, ...args) {
+  static info(author, msg, ...args) {
     const argsString = args ? args.map(arg => `${chalk.gray(`(${chalk.bold.blue(arg)})`)}`).join(' ') : '';
 
-    console.log(`${chalk.bold.gray(`[${chalk.bold.blue('~')}]`)} ${chalk.white(msg)} ${argsString}`)
+    console.log(`${chalk.bold.gray(`[${chalk.bold.blue('~')}]`)} ${chalk.bold.gray(`[${chalk.whiteBright(author)}]`)} ${chalk.white(msg)} ${argsString}`)
   }
 
-  static warn(msg, ...args) {
+  static warn(author, msg, ...args) {
     const argsString = args ? args.map(arg => `${chalk.gray(`(${chalk.bold.yellow(arg)})`)}`).join(' ') : '';
 
-    console.log(`${chalk.bold.bold.gray(`[${chalk.bold.yellow('!')}]`)} ${chalk.white(msg)} ${argsString}`)
+    console.log(`${chalk.bold.bold.gray(`[${chalk.bold.yellow('!')}]`)} ${chalk.bold.gray(`[${chalk.whiteBright(author)}]`)} ${chalk.white(msg)} ${argsString}`)
   }
 
-  static error(msg, ...args) {
+  static error(author, msg, ...args) {
     const argsString = args ? args.map(arg => `${chalk.gray(`(${chalk.bold.red(arg)})`)}`).join(' ') : '';
 
-    console.log(`${chalk.bold.gray(`[${chalk.bold.red('-')}]`)} ${chalk.white(msg)} ${argsString}`)
+    console.log(`${chalk.bold.gray(`[${chalk.bold.red('-')}]`)} ${chalk.bold.gray(`[${chalk.whiteBright(author)}]`)} ${chalk.white(msg)} ${argsString}`)
   }
 
-  static success(msg, ...args) {
+  static success(author, msg, ...args) {
     const argsString = args ? args.map(arg => `${chalk.gray(`(${chalk.bold.greenBright(arg)})`)}`).join(' ') : '';
 
-    console.log(`${chalk.bold.gray(`[${chalk.bold.greenBright('+')}]`)} ${chalk.white(msg)} ${argsString}`)
+    console.log(`${chalk.bold.gray(`[${chalk.bold.greenBright('+')}]`)} ${chalk.bold.gray(`[${chalk.whiteBright(author)}]`)} ${chalk.white(msg)} ${argsString}`)
   }
 }
 
